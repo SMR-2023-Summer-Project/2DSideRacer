2efeb69113137364efd13a0197976a5f38e283a6
Habib <73318032+MajorH5@users.noreply.github.com>
2023-07-08 14:58:51 -0400

    restructure & reorganize repository

---
+++ root/ui/title_screen/title_screen.gd
@@ -0,0 +1,13 @@
+# author: Haley Patel
+#Home Screen
+extends Control
+
+#2 Nodes: "Play" button, "Quit" Button
+
+#When player presses "Quit" everything shuts down
+func _on_quit_pressed():
+	get_tree().quit()
+  
+#When player presses "Play" the screen changes to the main screen where the game is
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://root/scenes/demo_scene/demo_scene.tscn")
