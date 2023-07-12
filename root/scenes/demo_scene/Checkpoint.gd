7ddb19b5e409345c7626521d1b904d2ce1b752b2
Coolr <Lilteemanjr@gmail.com>
2023-07-12 16:02:42 -0400

    Adds checkpoints to the multiplayer scene

--- root/scenes/demo_scene/Checkpoint.gd
+++ root/scenes/demo_scene/Checkpoint.gd
@@ -8,6 +8,6 @@ var x = 0
 func _on_body_entered(body: Node) -> void:
 	
 	if x > 0:
-		print(self.position)
+		print(self.position)#prints current position
 		Global.updated_spawn(self.position)
 	x += 1
