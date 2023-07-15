4bd4a47e940398751a56be659b079a361382607a
Coolr <Lilteemanjr@gmail.com>
2023-07-15 15:33:28 -0400

    Makes checkpoints easier to use
    This change to checkpoints makes it so that you can make them just by adding the node. Nothing extra is needed.

--- root/scenes/demo_scene/Checkpoint.gd
+++ root/scenes/demo_scene/Checkpoint.gd
@@ -1,9 +1,19 @@
 # author: Tolib
 extends Area2D
-# Ensures that the function only gets called after a player from the player group goes to a checkpoint
-class_name Checkpoint 
 # Sets the player spawnpoint to the place of the "checkpoint"
+#Makes this into a new node
+class_name Checkpoint 
+#makes the collision box
+var collisionShape = CollisionShape2D.new()
+#detects player
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
-		print(self.position)
-		body.update_spawn(self.position)
+		print(self.position)#prints the position of the checkpoint
+		body.update_spawn(self.position)#updates the players spawnpoint
+func _ready() -> void:
+	self.body_entered.connect(_on_body_entered)#makes the detection work
+	#makes the shape of the box, the size of it, and makes it a child of the checkpoint
+	collisionShape.shape = RectangleShape2D.new()
+	collisionShape.shape.extents = Vector2(20, 50)
+	add_child(collisionShape)
+
