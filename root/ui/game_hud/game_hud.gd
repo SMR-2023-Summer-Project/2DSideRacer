3abe09d6b3be5ec8d1947cff52a59ce79dbb43f0
Habib <73318032+MajorH5@users.noreply.github.com>
2023-07-19 16:57:01 -0400

    cleanup DS_Store files, fix HUD, move BG textures

--- root/ui/game_hud/game_hud.gd
+++ root/ui/game_hud/game_hud.gd
@@ -47,7 +47,52 @@ func hide_finished ():
 	
 	await finished.disappear()
 
+func _debug ():
+	var marker = create_player_marker("You")
+	
+	marker.set_inactive()
+	
+	var max_time = 10
+	var start = Time.get_unix_time_from_system()
+	
+	await play_countdown(3)
+	
+	marker.set_active()
+	
+	var x_position = 0
+	var goal = 60
+	
+	while true:
+		var current = Time.get_unix_time_from_system()
+		var delta_time = current - start
+		var game_time = clampf(max_time - delta_time, 0, max_time)
+		
+		set_remaining_time(game_time)
+		
+		if game_time == 0:
+			break
+		
+		x_position += randf()
+		x_position = clampf(x_position, 0, goal)
+		
+		marker.set_progress(x_position / goal)
+		
+		await Global.wait(0.15)
+	
+	marker.set_inactive()
+	
+	time_remaining.hide()
+	player_progress.hide()
+	
+	await show_finished()
+	
+	await Global.wait(3)
+	
+	await hide_finished()
+
 func _ready():
 	time_remaining.hide()
 	player_progress.hide()
 	countdown.hide()
+	
+	await _debug()
