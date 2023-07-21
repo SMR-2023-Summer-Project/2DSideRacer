8602a0a30a5b203d0c94398c770631835a038669
Coolr <Lilteemanjr@gmail.com>
2023-07-21 19:44:38 -0400

    Update player.gd

--- root/entities/player/player.gd
+++ root/entities/player/player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 const WALL_JUMP_VELOCITY = -300.0
 const DASH_SPEED = 800
 const DASH_DURATION = 0.2
-const DASH_COOLDOWN = 0.5
+const DASH_COOLDOWN = 0.35
 const CLIMB_SPEED = 100  # Adjust climbing speed as needed
 const WALL_JUMP_DELAY = 0.2  # The time window for wall jump after leaving the ground
 
@@ -51,7 +51,7 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("jump") and canDJ:
 			velocity.y = JUMP_VELOCITY * 0.75
 			canDJ = false
-	wall_jump_logic()
+	wall_jump()
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not Input.is_action_pressed("flip"):
 		# Reset double jump condition
@@ -180,7 +180,7 @@ func addCoins():
 	$UI.coin_count.text = str(coins)
 
 # Function to handle wall jumps
-func wall_jump_logic():
+func wall_jump():
 	if is_on_wall() and wallJumpTimer <= 0.0:
 		if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
 			if get_direction() != prevDirection:
