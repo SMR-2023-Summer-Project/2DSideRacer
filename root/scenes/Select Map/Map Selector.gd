1b018cd63d830d93a904a24595ae95bcdc4d4083
Coolr <Lilteemanjr@gmail.com>
2023-07-22 02:49:17 -0400

    g2

--- root/scenes/Select Map/Map Selector.gd
+++ root/scenes/Select Map/Map Selector.gd
@@ -3,14 +3,21 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 
-
-
+var back_button : BackButton
+var stage_Selection = []
 
 
 func _ready():
-	pass # Replace with function body.
-
-
+	stage_Selection.append( StageButton.new())
+	stage_Selection[0] = StageButton.new()
+	back_button = BackButton.new()
+	back_button.previousScene = "res://root/multiplayer/multiplayer.tscn"
+	stage_Selection[0].imgFile = 'res://root/assets/sprites/Selection Icons/Industrial Image.png'
+	stage_Selection[0].Scene = 'res://root/multiplayer/multiplayer.tscn'
+	add_child(stage_Selection[0])
+	add_child(back_button)
+	stage_Selection[0].position = Vector2(300,200)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
