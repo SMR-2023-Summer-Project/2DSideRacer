62bad1b6886f857ba9a40136351cca4da352807f
Habib <73318032+MajorH5@users.noreply.github.com>
2023-07-25 02:38:02 -0400

    rollback again

--- root/scenes/demo_scene_2/demo_scene_2.gd
+++ root/scenes/demo_scene_2/demo_scene_2.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+
 const Player = preload("res://root/entities/player/player.tscn")
 # Called when the node enters the scene tree for the first time.
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	var player = Player.instantiate()
-	player.name = str(multiplayer.get_unique_id())
-	add_child(player)
+	Global.updated_respawn_distance(750)
+	Global.updated_respawn(Vector2(200,585))
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- root/ui/game_hud/CoinCount.gd
+++ root/ui/game_hud/CoinCount.gd
@@ -1,11 +1,9 @@
-extends Node2D
+extends Label
 
-const Player = preload("res://root/entities/player/player.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player = Player.instantiate()
-	player.name = str(multiplayer.get_unique_id())
-	add_child(player)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
